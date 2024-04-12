f5c1639327bc3f4981ed10b6917055683610b197
Jonathan Rochat <jonathan.rochat@proton.me>
2024-04-12 19:49:55 +0200

    Ajout de la fonction main affichant pour l'instant un message 'hello world' et changement d'une erreur de syntaxe dans le fichier CMakeLists.txt du module core

---
+++ src/core/main.cpp
@@ -0,0 +1,12 @@
+/*
+-------------------------
+main.cpp
+auteur: Jonathan Rochat
+-------------------------
+Fichier contenant la fonction main.
+*/
+#include <iostream>
+
+void main() {
+    std::cout << "Hello, world !";
+}
