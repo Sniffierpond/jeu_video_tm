d2247f0c544f32f36877e6d9516e943b13107eb2
Jonathan Rochat <jonathan.rochat@proton.me>
2024-06-22 12:36:58 +0200

    Ajout de l'interface LevelInterface permettant d'agir sur un niveau

---
+++ include/contracts/LevelInterface.hpp
@@ -0,0 +1,29 @@
+/*
+    -------------------------
+    LevelInterface.hpp
+    auteur: Jonathan Rochat
+    -------------------------
+*/
+
+#pragma once
+
+#include <SFML/System/Vector2.hpp>
+
+#include "Block.hpp"
+#include "BlockGrid.hpp"
+#include "Player.hpp"
+
+class LevelInterface {
+    public:
+    virtual void update(int timeDiff) = 0;
+
+    virtual void select(sf::Vector2i blockPosition) = 0;
+    virtual void interact(sf::Vector2i blockPosition) = 0;
+    
+    virtual Block getBlock(sf::Vector2i) = 0;
+    virtual BlockGrid getBlocks(sf::Vector2i pos0, sf::Vector2i pos1) = 0;
+
+    virtual Player& getPlayer() = 0;
+
+    virtual unsigned int getLevelId();
+};
