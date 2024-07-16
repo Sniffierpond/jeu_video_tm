b2bd34fc0c90b566df42d3c6f449ae6890b7827e
Jonathan Rochat <jonathan.rochat@proton.me>
2024-07-16 08:54:13 +0400

    Implémentation de la classe TextureRegistry
    Classe basée sur la classe Registry permettant de stocker les références
    sur les textures utilisées par les différents éléments du jeu

---
+++ include/graphics/TextureRegistry.hpp
@@ -0,0 +1,13 @@
+/*
+    -------------------------
+    ExampleRenderer.hpp
+    auteur: Jonathan Rochat
+    -------------------------
+*/
+
+#pragma once
+
+#include "../utils/Registry.hpp"
+#include <SFML/Graphics/Texture.hpp>
+
+using TextureRegistry = Registry<sf::Texture>;
