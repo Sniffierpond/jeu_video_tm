a24a28a1510d4785fabbb3e22f4a2df57f522ffe
Jonathan Rochat <jonathan.rochat@proton.me>
2024-07-20 22:30:16 +0400

    Ajout de l'interface Updatable

---
+++ include/contracts/Updatable.hpp
@@ -0,0 +1,20 @@
+/*
+    -------------------------
+    Movable.hpp
+    auteur: Jonathan Rochat
+    -------------------------
+*/
+
+#pragma once
+
+#include <chrono>
+
+/*
+Défini une interface pour un objet dont l'état est amené à changer dans le temps.
+*/
+class Updatable {
+    /*
+    timeDifference: nombre de millisecondes écoulées depuis le dernier appel de la méthode
+    */
+    virtual void update(std::chrono::duration<int> timeDifference) = 0;
+};
