9c4d136fbe5b9b7026668a9ac2b10b9dea906e57
Jonathan Rochat <jonathan.rochat@proton.me>
2024-07-26 17:51:19 +0400

    Implémentation de la classe Acceleration
    Le type Acceleration est désormais décrit par une classe.

--- include/physics/Acceleration.hpp
+++ include/physics/Acceleration.hpp
@@ -9,16 +9,46 @@
 
 #include "Speed.hpp"
 #include <chrono>
-#include <utility>
 
-using Acceleration = std::pair<Speed, std::chrono::seconds>;
+class Acceleration {
+    private:
+    float acceleration_;
+    public:
+    enum Unit {bps2, bph2, kbps2, kbph2};
+
+    Acceleration() = default;
+    Acceleration(Speed speed, std::chrono::nanoseconds duration);
+    Acceleration(float acceleration, Unit unit);
+
+    /*
+    Retourne la vitesse dans l'unité indiquée.
+    */
+    float getAcceleration(Unit unit) const;
+
+    Acceleration& operator+=(const Acceleration& a);
+    Acceleration& operator-=(const Acceleration& a);
+    Acceleration& operator*=(float f);
+    Acceleration& operator/=(float f);
+};
+
+Acceleration operator-(const Acceleration& a);
+
+Acceleration operator+(const Acceleration& a1, const Acceleration& a2);
+Acceleration operator-(const Acceleration& a1, const Acceleration& a2);
+
+Acceleration operator*(const Acceleration& a, float f);
+Acceleration operator/(const Acceleration& a, float f);
+
+Speed operator*(const Acceleration& a, std::chrono::nanoseconds nanoseconds);
+
+std::ostream& operator<<(std::ostream& os, const Acceleration& acceleration);
 
 /*
 Retourne une accélération en blocs par seconde au carré.
 */
-Acceleration operator"" _bps2(long double speed);
+Acceleration operator"" _bps2(long double acceleration);
 
 /*
 Retourne une accélération en "kiloblocs" par heure au carré (un "kilobloc" étant équivalent à mille blocs).
 */
-Acceleration operator"" _kbph2(long double speed);
+Acceleration operator"" _kbph2(long double acceleration);
--- src/physics/Acceleration.cpp
+++ src/physics/Acceleration.cpp
@@ -7,10 +7,96 @@
 
 #include "../../include/physics/Acceleration.hpp"
 
-Acceleration operator"" _bps2(long double speed) {
-    return Acceleration(Speed(speed, std::chrono::seconds(1)), std::chrono::seconds(1));
+Acceleration::Acceleration(Speed speed, std::chrono::nanoseconds duration) {
+    acceleration_ = speed.getSpeed(Speed::bps) / duration.count() * 1'000'000'000; 
 }
 
-Acceleration operator"" _kbps2(long double speed) {
-    return Acceleration(Speed(1000 * speed, std::chrono::hours(1)), std::chrono::hours(1));
+Acceleration::Acceleration(float acceleration, Unit unit) {
+    switch (unit) {
+    case bps2:
+        acceleration_ = acceleration;
+        break;
+    case bph2:
+        acceleration_ = acceleration / 3600 / 3600;
+        break;
+    case kbps2:
+        acceleration_ = acceleration * 1000 * 1000;
+        break;
+    case kbph2:
+        acceleration_ = acceleration / 3.6 / 3.6;
+        break;
+    }
+}
+
+
+float Acceleration::getAcceleration(Unit unit) const {
+    switch (unit) {
+    case bps2:
+        return acceleration_;
+    case bph2:
+        return acceleration_ * 3600 * 3600;
+    case kbps2:
+        return acceleration_ / 1000 / 1000;
+    case kbph2:
+        return acceleration_ * 3.6 * 3.6;
+    }
+}
+
+
+Acceleration operator-(const Acceleration& a) {
+    return a * -1;
+}
+
+
+Acceleration& Acceleration::operator+=(const Acceleration& a) {
+    return *this = *this + a;
+}
+
+Acceleration& Acceleration::operator-=(const Acceleration& a) {
+    return *this = *this - a;
+}
+
+Acceleration& Acceleration::operator*=(float f) {
+    return *this = *this * f;
+}
+
+Acceleration& Acceleration::operator/=(float f) {
+    return *this = *this / f;
+}
+
+
+Acceleration operator+(const Acceleration& a1, const Acceleration& a2) {
+    return Acceleration(a1.getAcceleration(Acceleration::bps2) + a2.getAcceleration(Acceleration::bps2), Acceleration::bps2);
+}
+
+Acceleration operator-(const Acceleration& a1, const Acceleration& a2) {
+    return Acceleration(a1.getAcceleration(Acceleration::bps2) - a2.getAcceleration(Acceleration::bps2), Acceleration::bps2);
+}
+
+
+Acceleration operator*(const Acceleration& a, float f) {
+    return Acceleration(a.getAcceleration(Acceleration::bps2) * f, Acceleration::bps2);
+}
+
+Acceleration operator/(const Acceleration& a, float f) {
+    return Acceleration(a.getAcceleration(Acceleration::bps2) / f, Acceleration::bps2);
+}
+
+
+Speed operator*(const Acceleration& a, std::chrono::nanoseconds duration) {
+    return Speed(a.getAcceleration(Acceleration::bps2) * duration.count() / 1'000'000'000, Speed::bps);
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Acceleration& acceleration) {
+    return os << acceleration.getAcceleration(Acceleration::bps2) << "b/s²";
+}
+
+
+Acceleration operator"" _bps2(long double acceleration) {
+    return Acceleration(Speed(acceleration, std::chrono::seconds(1)), std::chrono::seconds(1));
+}
+
+Acceleration operator"" _kbps2(long double acceleration) {
+    return Acceleration(Speed(1000 * acceleration, std::chrono::hours(1)), std::chrono::hours(1));
 }
