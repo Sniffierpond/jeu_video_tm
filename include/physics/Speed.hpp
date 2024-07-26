d60a19ca68a0e778b2e77c5d0b9cba626d546188
Jonathan Rochat <jonathan.rochat@proton.me>
2024-07-26 17:49:30 +0400

    Implémentation de la classe Speed
    Le type 'Speed' est désormais décrit par une classe.

--- include/physics/Speed.hpp
+++ include/physics/Speed.hpp
@@ -9,9 +9,43 @@
 
 #include <SFML/System/Vector2.hpp>
 #include <chrono>
-#include <utility>
+#include <iostream>
 
-using Speed = std::pair<float, std::chrono::seconds>;
+class Acceleration;
+
+class Speed {
+    private:
+    float speed_;
+    public:
+    enum Unit {bps, bph, kbps, kbph};
+
+    Speed() = default;
+    Speed(float distance, std::chrono::nanoseconds duration);
+    Speed(float speed, Unit unit);
+
+    /*
+    Retourne la vitesse dans l'unité indiquée.
+    */
+    float getSpeed(Unit unit) const;
+
+    Speed& operator+=(const Speed& v);
+    Speed& operator-=(const Speed& v);
+    Speed& operator*=(float f);
+    Speed& operator/=(float f);
+};
+
+Speed operator-(const Speed& s);
+
+Speed operator+(const Speed& s1, const Speed& s2);
+Speed operator-(const Speed& s1, const Speed& s2);
+
+Speed operator*(const Speed& s, float f);
+Speed operator/(const Speed& s, float f);
+
+float operator*(const Speed& s, std::chrono::nanoseconds duration);
+Acceleration operator/(const Speed& s, std::chrono::nanoseconds duration);
+
+std::ostream& operator<<(std::ostream& os, const Speed& speed);
 
 /*
 Retourne une vitesse en blocs par seconde.
--- src/physics/Speed.cpp
+++ src/physics/Speed.cpp
@@ -6,7 +6,97 @@
 */
 
 #include "../../include/physics/Speed.hpp"
+#include "../../include/physics/Acceleration.hpp"
 #include <SFML/System/Vector2.hpp>
+
+Speed::Speed(float distance, std::chrono::nanoseconds duration) {
+    speed_ = distance / duration.count() * 1'000'000'000;
+}
+
+Speed::Speed(float speed, Unit unit) {
+    switch (unit) {
+    case bps:
+        speed_ = speed;
+        break;
+    case bph:
+        speed_ = speed / 3600;
+        break;
+    case kbps:
+        speed_ = speed * 1000;
+        break;
+    case kbph:
+        speed_ = speed / 3.6;
+        break;
+    }
+}
+
+
+float Speed::getSpeed(Unit unit) const {
+    switch (unit) {
+    case bps:
+        return speed_;
+    case bph:
+        return speed_ * 3600;
+    case kbps:
+        return speed_ / 1000;
+    case kbph:
+        return speed_ * 3.6;
+    }
+}
+
+
+Speed operator-(const Speed& s) {
+    return s * -1;
+}
+
+Speed& Speed::operator+=(const Speed& s) {
+    return *this = *this + s;
+}
+
+Speed& Speed::operator-=(const Speed& s) {
+    return *this = *this - s;
+}
+
+Speed& Speed::operator*=(float f) {
+    return *this = *this * f;
+}
+
+Speed& Speed::operator/=(float f) {
+    return *this = *this / f;
+}
+
+
+Speed operator+(const Speed& s1, const Speed& s2) {
+    return Speed(s1.getSpeed(Speed::bps) + s2.getSpeed(Speed::bps), Speed::bps);
+}
+
+Speed operator-(const Speed& s1, const Speed& s2) {
+    return Speed(s1.getSpeed(Speed::bps) - s2.getSpeed(Speed::bps), Speed::bps);
+}
+
+
+Speed operator*(const Speed& s1, float f) {
+    return Speed(s1.getSpeed(Speed::bps) * f, Speed::bps);
+}
+
+Speed operator/(const Speed& s1, float f) {
+    return Speed(s1.getSpeed(Speed::bps) / f, Speed::bps);
+}
+
+
+float operator*(const Speed& s1, std::chrono::nanoseconds duration) {
+    return s1.getSpeed(Speed::bps) * duration.count() / 1'000'000'000;
+}
+
+Acceleration operator/(const Speed& s1, std::chrono::nanoseconds duration) {
+    return Acceleration(s1.getSpeed(Speed::bps) / duration.count() * 1'000'000'000, Acceleration::bps2);
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Speed& speed) {
+    return os << speed.getSpeed(Speed::bps) << "b/s";
+}
+
 
 Speed operator"" _bps(long double speed) {
     return Speed(speed, std::chrono::seconds(1));
