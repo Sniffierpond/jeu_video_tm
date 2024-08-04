16e8cc1d90e89e647b5cccbcf31f26e15375767e
Jonathan Rochat <jonathan.rochat@proton.me>
2024-08-04 19:24:13 +0300

    Changement de la façon dont sont stockés les objets T dans la classe générique Registry A présent, Registry stocke des 'unique-ptr' vers les objets T qui lui sont soummis. Cela permet de pouvoir ajouter des classes dérivées des objets T, réglant ainsi les problèmes du type BlockRegistry.

--- include/utils/Registry.hpp
+++ include/utils/Registry.hpp
@@ -10,34 +10,37 @@
 #include <map>
 #include <stdexcept>
 #include <string>
+#include <memory>
 
 /*
 Représente un registre stockant des références sur des objets associées à des identifiants
 sous forme de chaîne de caractères
 */
-template <typename T>
+template <typename T1>
 class Registry {
     private:
-    std::map<std::string, T> registry_;
+    std::map<std::string, std::unique_ptr<T1>> registry_;
+
     public:
     /*
     Enregistre la copie d'un élément (t) à l'aide de son identifiant (id)
     */
-    void registerElement(const std::string& id, const T& t) {
+    template<typename T2>
+    void registerElement(const std::string& id, const T2& t) {
         if (registry_.find(id) != registry_.end()) {
             throw std::runtime_error("Error: the element cannot be registered because it already is.");
         }
         else {
-            registry_[id] = t;
+            registry_[id] = std::make_unique<T2>(t);
         }
     }
     /*
     Récupère un élément à l'aide de son identifiant (id)
     */
-    const T& get(const std::string& id) const {
+    const T1& get(const std::string& id) const {
         if (registry_.find(id) == registry_.end()) {
             throw std::runtime_error("Error: The element with the given id isn't registered");
         }
-        return registry_.at(id);
+        return *registry_.at(id);
     }
 };
