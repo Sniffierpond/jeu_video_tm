91d2986240ab50e39bb9a52bef8ab6b931acea0a
Jonathan Rochat <jonathan.rochat@proton.me>
2024-08-04 08:33:06 +0300

    Changement de la fonction de la classe View
    A la base, la classe View devait servir uniquement à représenter une
    zone graphique et à gérer des Renderer qui ensuite devaient eux-mêmes
    adapter leur rendu à la zone graphique. Après réflexion, la fonction de
    la classe View a été repensée: Désormais, elle se charge elle-même
    d'adapter les textures qui lui sont soumises par ses Renderers à la zone
    graphique qu'elle couvre. Cela permet ainsi de limiter les
    préoccupations de devoir adapter le rendu à la fenêtre du jeu à cette
    classe.

--- include/graphics/View.hpp
+++ include/graphics/View.hpp
@@ -7,26 +7,34 @@
 #pragma once
 
 #include "../contracts/Viewable.hpp"
+#include <SFML/Graphics/Texture.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 
 class View: Viewable {
     private:
+    sf::RenderTarget* renderTarget_;
+
     Renderer* renderer_;
 
     unsigned int width_;
     unsigned int height_;
 
-    sf::Vector2f windowPos0_;
+    sf::Vector2i windowPos0_;
 
     public:
-    View(Renderer& renderer, unsigned int width, unsigned int height, sf::Vector2f windowPos0);
+    View(sf::RenderTarget& renderTarget, Renderer& renderer, unsigned int width, unsigned int height, sf::Vector2i windowPos0);
 
     void setRenderer(Renderer& renderer) override;
 
+    /*
+    Récupère une texture du Renderer et l'adapte à la zone graphique qu'elle doit couvrir
+    en agrandissant cette texture et en tronquant les parties externes de la texture nécessaires.
+    */
     void refresh() override;
 
     unsigned int getWidth() override;
     unsigned int getHeight() override;
 
-    sf::Vector2f getWindowPos0() override;
+    sf::Vector2i getWindowPos0() override;
 };
--- src/graphics/View.cpp
+++ src/graphics/View.cpp
@@ -6,17 +6,49 @@
 */
 
 #include "../../include/graphics/View.hpp"
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <iostream>
 
-View::View(Renderer& renderer, unsigned int width, unsigned int height, sf::Vector2f windowPos0): renderer_(&renderer), width_(width), height_(height), windowPos0_(windowPos0) {
-    renderer_->setViewable(*this);
-}
+View::View(sf::RenderTarget& renderTarget, Renderer& renderer, unsigned int width, unsigned int height, sf::Vector2i windowPos0):renderTarget_(&renderTarget), renderer_(&renderer), width_(width), height_(height), windowPos0_(windowPos0) {}
 
 void View::setRenderer(Renderer& renderer) {
     renderer_ = &renderer;
 }
 
 void View::refresh() {
-    renderer_->render();
+    float rectHeight;
+    float rectWidth, x, y;
+
+    float width = renderTarget_->mapCoordsToPixel(sf::Vector2f(width_, 0)).x;
+    float height = renderTarget_->mapCoordsToPixel(sf::Vector2f(0, height_)).y;
+
+
+    sf::Texture texture = renderer_->render();
+
+    if (texture.getSize().x / 1.0 / width * height >= texture.getSize().y) {
+        rectHeight = texture.getSize().y;
+        rectWidth = texture.getSize().y / 1.0 / height * width;
+        x = (texture.getSize().x - rectWidth) / 2;
+        y = 0;
+    }
+    else {
+        rectWidth = texture.getSize().x;
+        rectHeight = texture.getSize().x / 1.0 / width * height;
+        y = (texture.getSize().y - rectHeight) / 2;
+        x = 0;
+    }
+
+    sf::Sprite sprite(texture, sf::IntRect(x, y, rectWidth, rectHeight));
+
+    sprite.setScale(width_ / 1.0 / sprite.getTextureRect().width, height_ / 1.0 / sprite.getTextureRect().height);
+
+    sprite.setPosition(sf::Vector2f(windowPos0_));
+
+    renderTarget_->draw(sprite);
 }
 
 unsigned int View::getWidth() {
@@ -27,6 +59,6 @@ unsigned int View::getHeight() {
     return height_;
 }
 
-sf::Vector2f View::getWindowPos0() {
+sf::Vector2i View::getWindowPos0() {
     return windowPos0_;
 }
