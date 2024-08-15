793337972ba4a72b919de3484e95eb8506d76438
Jonathan Rochat <jonathan.rochat@proton.me>
2024-08-15 19:06:34 +0200

    Ajouts à la classe GameApplication
    - modification de la méthode launch pour contrôler ses exceptions
    - ajout de l'état PlayingGameState qui n'est pas encore défini
    - amélioration des performances
    - affichage d'une fenêtre

--- src/core/GameApplication.cpp
+++ src/core/GameApplication.cpp
@@ -6,27 +6,45 @@
 */
 
 #include "../../include/core/GameApplication.hpp"
+#include "../../include/core/PlayingGameState.hpp"
+#include <SFML/Window/WindowStyle.hpp>
 #include <chrono>
 #include <cstdlib>
+#include <iostream>
+#include <thread>
+#include <SFML/Graphics.hpp>
 
 GameApplication::GameApplication(const cxxopts::ParseResult& options) {
     fps_ = options["fps"].as<unsigned int>();
 }
 
-int GameApplication::launch() {
-    std::chrono::steady_clock clock;
-    auto start = clock.now();
+int GameApplication::launch() noexcept {
+    try {
+        std::chrono::steady_clock clock;
+        auto start = clock.now();
 
-    while (stack_.size() > 0 || !shouldExit_) {
-        if (clock.now() - start < std::chrono::seconds(1) / 1.0 / fps_) {
-            continue;
+        sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "Jeu TM", sf::Style::Fullscreen);
+
+        stack_.push(PlayingGameState(window));
+
+        while (stack_.size() > 0 || !shouldExit_) {
+            if (clock.now() - start < std::chrono::seconds(1) / 1.0 / fps_) {
+                std::this_thread::sleep_for(std::chrono::seconds(1) / 1.0 / fps_ - (clock.now() - start));
+                continue;
+            }
+      
+            stack_.update();
+            window.display();
+
+            start = clock.now();
         }
 
-        stack_.update();
-        start = clock.now();
+        return EXIT_SUCCESS;
     }
-
-    return EXIT_SUCCESS;
+    catch (...) {
+        std::cerr << "An unknown exception occured." << std::endl;
+        return EXIT_FAILURE;
+    }
 }
 
 void GameApplication::exit() {
