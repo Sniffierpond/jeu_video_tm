5b02cd299c99b18a13557eb84d7e4253587d2188
Jonathan Rochat <jonathan.rochat@proton.me>
2024-08-18 22:18:02 +0200

    Modification du modèle Array2
    Ajout d'un nouveau constructeur et modification du l'identifiant du
    membre 'vector_' en un nom plus adapté

--- include/utils/Array2.hpp
+++ include/utils/Array2.hpp
@@ -12,22 +12,24 @@
 template <typename T>
 class Array2 {
     private:
-    std::vector<std::vector<T>> vector_;
+    std::vector<std::vector<T>> array_;
     sf::Vector2i origin_;
 
     public:
     Array2(sf::Vector2i origin, unsigned int width, unsigned int height, T fill = T()): origin_(origin) {
-        vector_.resize(height);
+        array_.resize(height);
         
-        for (auto element : vector_) {
+        for (auto element : array_) {
             for (int i = 0; i < width; ++i) {
                 element.push_back(fill);
             }
         }
     }
 
+    Array2(const std::vector<std::vector<T>>& array, sf::Vector2i origin): array_(array), origin_(origin) {}
+
     T& at(int x, int y) {
-        return vector_.at(origin_.y + y).at(origin_.x + x);
+        return array_.at(origin_.y + y).at(origin_.x + x);
     }
 
     const T& at(int x, int y) const {
