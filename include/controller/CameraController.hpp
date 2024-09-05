7fd54a6bafe014e59bcb33a68d5554a032bed1e7
Jonathan Rochat <jonathan.rochat@proton.me>
2024-09-05 12:35:50 +0200

    Ajout de la classe CameraController (Note: contient possiblement des bugs)

---
+++ include/controller/CameraController.hpp
@@ -0,0 +1,46 @@
+/*
+    -------------------------
+    CameraController.hpp
+    auteur: Jonathan Rochat
+    -------------------------
+*/
+
+#pragma once
+
+#include "../contracts/Controller.hpp"
+#include "../graphics/MovableCamera.hpp"
+#include <chrono>
+
+class CameraController: Controller {
+    private:
+    InputHandler* inputHandler_;
+    Viewable* viewable_;
+    MovableCamera* camera_;
+
+    bool stopped_ = false;
+
+    std::chrono::steady_clock clock_;
+    std::chrono::time_point<std::chrono::steady_clock> start_;
+
+    public:
+    CameraController(MovableCamera& camera, InputHandler& inputHandler, Viewable& viewable);
+
+    void setInputHandler(InputHandler& inputHandler) override;
+    void setViewable(Viewable& viewable) override;
+
+    void refresh() override;
+
+    void update() override;
+
+    void pressKey(sf::Keyboard::Key keyCode) override;
+    void releaseKey(sf::Keyboard::Key keyCode) override;
+
+    void leftClick(sf::Vector2f position) override;
+    void rightClick(sf::Vector2f position) override;
+
+    void moveMouse(sf::Vector2f position) override;
+
+    bool stopped() const override;
+
+    void exit() override;
+};
---
+++ src/controller/CameraController.cpp
@@ -0,0 +1,95 @@
+/*
+    -------------------------
+    CameraController.cpp
+    auteur: Jonathan Rochat
+    -------------------------
+*/
+
+#include "../../include/controller/CameraController.hpp"
+#include "../../include/contracts/GameState.hpp"
+#include <SFML/Window/Keyboard.hpp>
+
+CameraController::CameraController(MovableCamera& camera, InputHandler& inputHandler, Viewable& viewable): camera_(&camera), inputHandler_(&inputHandler), viewable_(&viewable) {
+    start_ = clock_.now();
+    inputHandler_->setController(*this);
+}
+
+
+void CameraController::setInputHandler(InputHandler& inputHandler) {
+    inputHandler_ = &inputHandler;
+    inputHandler_->setController(*this);
+}
+
+void CameraController::setViewable(Viewable& viewable) {
+    viewable_ = &viewable;
+}
+
+
+void CameraController::refresh() {
+    if (!stopped_) {
+        viewable_->refresh();
+    }
+}
+
+
+void CameraController::update() {
+    if (!stopped_) {
+        auto timeDiff = clock_.now() - start_;
+        start_ = clock_.now();
+
+        std::cout << &camera_ << std::endl;
+        auto a = *camera_;
+        camera_->movementHandler().update(timeDiff);
+        
+        inputHandler_->handleInputs();
+
+        refresh();
+    }
+}
+
+
+void CameraController::pressKey(sf::Keyboard::Key keyCode) {
+    if (keyCode == sf::Keyboard::W || keyCode == sf::Keyboard::Up) {
+        camera_->movementHandler().addSpeed(sf::Vector2<Speed>(0.0_bps, 2.0_bps));
+    }
+    else if (keyCode == sf::Keyboard::S || keyCode == sf::Keyboard::Down) {
+        camera_->movementHandler().addSpeed(sf::Vector2<Speed>(0.0_bps, -2.0_bps));
+    }
+    else if (keyCode == sf::Keyboard::D || keyCode == sf::Keyboard::Right) {
+        camera_->movementHandler().addSpeed(sf::Vector2<Speed>(2.0_bps, 0.0_bps));
+    }
+    else if (keyCode == sf::Keyboard::A || keyCode == sf::Keyboard::Left) {
+        camera_->movementHandler().addSpeed(sf::Vector2<Speed>(-2.0_bps, 0.0_bps));
+    }
+}
+
+void CameraController::releaseKey(sf::Keyboard::Key keyCode) {
+    if (keyCode == sf::Keyboard::W || keyCode == sf::Keyboard::Up) {
+        camera_->movementHandler().addSpeed(sf::Vector2<Speed>(0.0_bps, -2.0_bps));
+    }
+    else if (keyCode == sf::Keyboard::S || keyCode == sf::Keyboard::Down) {
+        camera_->movementHandler().addSpeed(sf::Vector2<Speed>(0.0_bps, 2.0_bps));
+    }
+    else if (keyCode == sf::Keyboard::D || keyCode == sf::Keyboard::Right) {
+        camera_->movementHandler().addSpeed(sf::Vector2<Speed>(-2.0_bps, 0.0_bps));
+    }
+    else if (keyCode == sf::Keyboard::A || keyCode == sf::Keyboard::Left) {
+        camera_->movementHandler().addSpeed(sf::Vector2<Speed>(2.0_bps, 0.0_bps));
+    }
+}
+
+
+void CameraController::leftClick(sf::Vector2f position) {}
+
+void CameraController::rightClick(sf::Vector2f position) {}
+
+
+void CameraController::moveMouse(sf::Vector2f position) {}
+
+
+bool CameraController::stopped() const {
+    return stopped_;
+}
+void CameraController::exit() {
+    stopped_ = true;
+}
