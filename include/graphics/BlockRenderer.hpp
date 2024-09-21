49af62dba619fa1c1040d75a2f49149def4d9a53
Jonathan Rochat <jonathan.rochat@proton.me>
2024-09-21 15:53:38 +0200

    En attendant d'avoir diagnostiqué précisément le problème, le membre grid_ de BlockRenderer sera copié.

--- include/graphics/BlockRenderer.hpp
+++ include/graphics/BlockRenderer.hpp
@@ -12,6 +12,7 @@
 #include "TextureRegistry.hpp"
 #include <SFML/Graphics/Rect.hpp>
 #include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/Vector2.hpp>
 #include "../game/BlockGrid.hpp"
 
 /*
@@ -19,12 +20,15 @@ Effectue le rendu d'une grille de blocs.
 */
 class BlockRenderer: public Renderer {
     private:
-    const BlockGrid& grid_;
+    const BlockGrid grid_;
     const TextureRegistry& textureRegistry_;
     sf::FloatRect renderArea_;
+    sf::Vector2f cameraPos_;
 
     public:
     BlockRenderer(const BlockGrid& grid, sf::FloatRect renderArea, const TextureRegistry& registry);
 
     sf::Texture render() const override;
+
+    void setCameraPos(sf::Vector2f pos) {cameraPos_ = pos;}
 };
