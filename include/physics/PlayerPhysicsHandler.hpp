8e6e4605115a5120ba4150209e8cb74e0064f19e
Jonathan Rochat <jonathan.rochat@proton.me>
2024-10-13 14:09:33 +0200

    Ajout de la classe PlayerPhysicsHandler gérant les mouvements du joueur.

---
+++ include/physics/PlayerPhysicsHandler.hpp
@@ -0,0 +1,47 @@
+/*
+    -------------------------
+    PlayerPhysicsHandler.hpp
+    auteur: Jonathan Rochat
+    -------------------------
+*/
+
+#pragma once
+
+#include "PhysicsHandler.hpp"
+#include "../game/Player.hpp"
+#include <chrono>
+
+class PlayerPhysicsHandler: public PhysicsHandler {
+    private:
+    Player& player_;
+    Speed jumpSpeed_;
+
+    bool goingLeft_ = false;
+    bool goingRight_ = false;
+    bool jumping_ = false;
+    bool slowingDown_ = false;
+
+    public:
+    PlayerPhysicsHandler(Player& player, Acceleration gravityAcceleration, Speed maxHorSpeed, Speed maxVertSpeed, Speed jumpSpeed, const Level& level);
+
+    void update(std::chrono::nanoseconds timeDifference) override;
+
+    /*
+    Fait débuter un mouvement vers la gauche au joueur.
+    */
+    void startGoingLeft();
+    /*
+    Fait débuter un mouvement vers la droite au joueur.
+    */
+    void startGoingRight();
+
+    /*
+    Le fait s'arrêter.
+    */
+    void stop();
+
+    /*
+    Le fait sauter.
+    */
+    void jump();
+};
---
+++ src/physics/PlayerPhysicsHandler.cpp
@@ -0,0 +1,100 @@
+/*
+    -------------------------
+    PlayerPhysicsHandler.cpp
+    auteur: Jonathan Rochat
+    -------------------------
+*/
+
+#include "../../include/physics/PlayerPhysicsHandler.hpp"
+#include <SFML/System/Vector2.hpp>
+#include <cmath>
+
+PlayerPhysicsHandler::PlayerPhysicsHandler(Player& player, Acceleration gravityAcceleration, Speed maxHorSpeed, Speed maxVertSpeed, Speed jumpSpeed, const Level& level):
+    player_(player),
+    jumpSpeed_(jumpSpeed),
+    PhysicsHandler(gravityAcceleration, maxHorSpeed, maxVertSpeed, {-0.5, 0, 1, 2}, player.movementHandler(), level)
+{}
+
+void PlayerPhysicsHandler::update(std::chrono::nanoseconds timeDifference) {
+    PhysicsHandler::update(timeDifference);
+
+    sf::Vector2<Speed> playerSpeed = player_.movementHandler().getSpeed();
+    
+    if (slowingDown_) {
+        // contrôle que le joueur est sur une surface solide pour le faire ralentir
+        if (collides({player_.movementHandler().getPosition().x, std::ceil(player_.movementHandler().getPosition().y - 1)})) {
+            if (goingLeft_) {
+                player_.movementHandler().setAcceleration({4.0_bps2, player_.movementHandler().getAcceleration().y});
+            }
+            else if (goingRight_) {
+                player_.movementHandler().setAcceleration({-4.0_bps2, player_.movementHandler().getAcceleration().y});
+            }
+        }
+        // si ce n'est pas le cas, arrête la déccélération du joueur
+        else {
+            player_.movementHandler().setAcceleration({0.0_bps2, player_.movementHandler().getAcceleration().y});
+        }
+
+        // si le joueur était en train de ralentir et qu'il a atteint une vitesse nulle ou inverse à la direction dans laquelle il allait, arrête la déccélération
+        // et rend la vitesse nulle.
+        if ((goingLeft_ && playerSpeed.x.getSpeed(Speed::bps) >= 0) || (goingRight_ && playerSpeed.x.getSpeed(Speed::bps) <= 0)) {
+            player_.movementHandler().setSpeed({0.0_bps, playerSpeed.y});
+            player_.movementHandler().setAcceleration({0.0_bps2, player_.movementHandler().getAcceleration().y});
+
+            goingLeft_ = false;
+            goingRight_ = false;
+            slowingDown_ = false;
+        }
+    }
+    
+    if (playerSpeed.y.getSpeed(Speed::bps) <= 0) {
+        jumping_ = false;
+
+        if (playerSpeed.y.getSpeed(Speed::bps) < 0) {
+            player_.status() = Player::FALLING;
+        }
+        else {
+            if (playerSpeed.x.getSpeed(Speed::bps) == 0) {
+                player_.status() = Player::STANDING;
+            }
+            else {
+                player_.status() = Player::WALKING;
+            }
+        }
+    }
+    else {
+        player_.status() = Player::JUMPING;
+    }
+}
+
+void PlayerPhysicsHandler::startGoingLeft() {
+    if (!goingLeft_ || slowingDown_) {
+        goingLeft_ = true;
+        goingRight_ = false;
+        slowingDown_ = false;
+
+        player_.movementHandler().setAcceleration({-2.0_bps2, player_.movementHandler().getAcceleration().y});
+    }
+}
+
+void PlayerPhysicsHandler::startGoingRight() {
+    if (!goingRight_ || slowingDown_) {
+        goingLeft_ = false;
+        goingRight_ = true;
+        slowingDown_ = false;
+
+        player_.movementHandler().setAcceleration({2.0_bps2, player_.movementHandler().getAcceleration().y});
+    }
+}
+
+void PlayerPhysicsHandler::stop() {
+    slowingDown_ = true;
+}
+
+void PlayerPhysicsHandler::jump() {
+    // fait sauter le joueur uniquement s'il se trouve sur une surface solide (pas déjà dans les airs)
+    if (collides({player_.movementHandler().getPosition().x, std::ceil(player_.movementHandler().getPosition().y - 1)})) {
+        player_.movementHandler().addSpeed({player_.movementHandler().getSpeed().x, jumpSpeed_});
+        jumping_ = true;
+    }
+}
